ad5d428bc975fd8247d534189d02ee2100fcd608
Zubair Moosa <mzmoosa01@gmail.com>
2021-06-06 17:12:42 +0200

    Feat: Helper class and DTO

---
+++ digitalAlbumApi/DTOs/UserDtos/CreateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digitalAlbumApi.DTOs.UserDtos
+{
+    public class CreateUserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
