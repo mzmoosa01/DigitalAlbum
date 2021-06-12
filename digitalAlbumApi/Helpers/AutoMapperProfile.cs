76c4882e6790e699df3a8549f38f6632076e5300
Zubair Moosa <mzmoosa01@gmail.com>
2021-06-12 17:04:25 +0200

    Feat: startup configuration

---
+++ digitalAlbumApi/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using digitalAlbumApi.DTOs.UserDtos;
+using digitalAlbumApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digitalAlbumApi.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, CreateUserDto>();
+        }
+    }
+}
