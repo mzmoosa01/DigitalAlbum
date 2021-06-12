6ba410591b49b4d7d8d6d017d847df6e7d492471
Zubair Moosa <mzmoosa01@gmail.com>
2021-06-12 17:22:18 +0200

    Feat: register route

--- digitalAlbumApi/Controllers/UsersController.cs
+++ digitalAlbumApi/Controllers/UsersController.cs
@@ -1,4 +1,10 @@
-﻿using digitalAlbumApi.Models;
+﻿using AutoMapper;
+using digitalAlbumApi.DTOs.UserDtos;
+using digitalAlbumApi.Helpers;
+using digitalAlbumApi.Interfaces;
+using digitalAlbumApi.Models;
+using digitalAlbumApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,15 +14,40 @@ using System.Threading.Tasks;
 
 namespace digitalAlbumApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private readonly AlbumContext _context;
+        private IUserService _userService;
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
 
-        public UsersController(AlbumContext context)
+        public UsersController(IUserService userService, IMapper mapper, AppSettings appSettings)
         {
-            _context = context;
+            _userService = userService;
+            _mapper = mapper;
+            _appSettings = appSettings;
         }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] CreateUserDto model)
+        {
+            var user = _mapper.Map<User>(model);
+
+            try
+            {
+                _userService.CreateUser(user, model.Password);
+                return Ok();
+            }
+            catch(AppException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+
+
     }
 }
